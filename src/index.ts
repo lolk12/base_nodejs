77d9ea9247255f02f8750f36ae1f081bcd3e1d75
Alexey Kolpakov <i@alexey-kolpakov.ru>
2022-06-06 21:02:04 +0300

    remove trash

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
-import TelegramBot from 'node-telegram-bot-api';
-
-import { start as startBot } from 'controller/chat/start';
-import { start as startDB } from 'model/db/start';
-import { errorLogger } from 'helpers/logger';
-
-const getUserName = (msg: TelegramBot.Message) => msg?.chat?.username || '';
-
 const main = async () => {
-  try {
-    const client = await startDB();
-    startBot(client);
-  } catch (e) {
-    errorLogger(e);
-  }
+  console.log('START APP');
 };
 
 main();
